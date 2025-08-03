1274c1fbbace750ca6d4db667a4acbf82796346f
utsxvrai <utsavrai2115@gmail.com>
2025-08-03 11:38:18 +0530

    added the upstash redis key

--- backend/badminton-service/src/config/redis-config.js
+++ backend/badminton-service/src/config/redis-config.js
@@ -1,5 +1,5 @@
 const Redis = require('ioredis'); // Note: Capital R for class
-const redis = new Redis();
+const redis = new Redis(process.env.REDIS_URL);
 
 redis.on('connect', () => {
   console.log('Connected to Redis');
